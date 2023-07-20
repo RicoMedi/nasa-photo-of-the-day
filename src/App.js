ea67c83013facbadb42f55f77ad37e864e98ccaf
Madcodes831 <r.medina2119@gmail.com>
2023-07-20 14:06:40 -0700

    All done with part 2

--- src/App.js
+++ src/App.js
@@ -10,15 +10,17 @@ function App() {
   useEffect(() =>{
  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
     .then(res => {
-      setData(res.data);
+      setData(res.data)
     })
     .catch(err => console.error(err))
   }, [])
   
   return (
-    <div className="App">
-      {data && <NasaPhoto photo={data}/> }
-    </div>
+    <section>
+      <div className="App">
+        {data && <NasaPhoto photo={data}/> }
+      </div>
+    </section>
   );
 }
 
--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,14 +1,53 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const StyledCard= styled.div`
+border: 1px solid black;
+border-radius: 12px
+background-color: :
+}}
+
+`
+const ImageContainer = styled.img`
+border: 4px solid white
+`
+const Header = styled.h2`
+color: white;
+text-shadow: -1px 1px 0 #000,
+                          1px 1px 0 #000,
+                         1px -1px 0 #000,
+                        -1px -1px 0 #000;
+font-size: 40px;
+
+`
+const Date = styled.p`
+font-size: 30px;
+color: white;
+text-shadow: -1px 1px 0 #000,
+                          1px 1px 0 #000,
+                         1px -1px 0 #000,
+                        -1px -1px 0 #000;
+`
+const Paragraph = styled.p`
+font-size: 18px;
+padding: 10px;
+border: 4px solid white;
+background: white;
+font-family: Lucida Console;
+direction: ltr;
+
+
+`
 
 const NasaPhoto = (props) => {
-      
+
     return (
-      <div className= 'nasa-photo-wrapper'>
-        <h3>{props.photo.title}</h3>
-        <p>{props.photo.date}</p>
-        <img src={props.photo.hdurl}/>
-        <p className='main-photo-explanation'>{props.photo.explanation}</p>
-      </div> 
+      <StyledCard className= 'nasa-photo-wrapper'>
+        <Header>{props.photo.title}</Header>
+        <ImageContainer src={props.photo.hdurl}/>
+        <Date>{props.photo.date}</Date>
+        <Paragraph className='main-photo-explanation'>{props.photo.explanation}</Paragraph>
+      </StyledCard> 
     )
 }
  
